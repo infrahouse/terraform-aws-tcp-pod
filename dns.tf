9163d83389005aa9835c2a355c051c6b32aa619a
xOtanix <maksym@tinyfish.io>
2025-08-29 12:12:34 +0300

    add caa record

--- dns.tf
+++ dns.tf
@@ -10,3 +10,18 @@ resource "aws_route53_record" "extra" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "caa_amazon" {
+  provider = aws.dns
+  count    = var.create_caa_records ? length(local.dns_a_records) : 0
+  zone_id  = var.zone_id
+  name     = trimprefix(join(".", [local.dns_a_records[count.index], data.aws_route53_zone.selected.name]), ".")
+  type     = "CAA"
+  ttl      = 300
+  records = [
+    "0 issue \"amazon.com\"",
+    "0 issue \"amazontrust.com\"",
+    "0 issue \"awstrust.com\"",
+    "0 issue \"amazonaws.com\""
+  ]
+}
