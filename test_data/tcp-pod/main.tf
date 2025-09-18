d97e35b02c81abcf5561a21ea8b5a0177df5f226
xOtanix <maksym@tinyfish.io>
2025-09-18 17:28:45 +0300

    update test data(using latest cloud-init)

--- test_data/tcp-pod/main.tf
+++ test_data/tcp-pod/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 module "jumphost-cloud-init" {
   source                   = "registry.infrahouse.com/infrahouse/cloud-init/aws"
-  version                  = "1.18.0"
+  version                  = "2.2.0"
   environment              = local.environment
   role                     = "jumphost"
   puppet_hiera_config_path = "/opt/infrahouse-puppet-data/environments/${local.environment}/hiera.yaml"
