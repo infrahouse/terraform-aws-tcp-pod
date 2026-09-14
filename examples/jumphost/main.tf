e97bbc6d55db5234d52ce9dc1ff1d8fdd9918d88
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2026-09-14 13:49:06 -0700

    chore: bump version to 1.2.0

--- examples/jumphost/main.tf
+++ examples/jumphost/main.tf
@@ -38,7 +38,7 @@ module "jumphost-cloud-init" {
 
 module "jumphost" {
   source  = "registry.infrahouse.com/infrahouse/tcp-pod/aws"
-  version = "1.1.0"
+  version = "1.2.0"
   providers = {
     aws     = aws
     aws.dns = aws
